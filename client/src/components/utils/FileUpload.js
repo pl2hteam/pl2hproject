be097b5ade0875784f7970388c8c2a84706ea4b4
park jun hyuk <89543695+berrypjh@users.noreply.github.com>
2021-11-05 11:30:24 +0900

    새로운 수정

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Dropzone from "react-dropzone";
-import { Icon } from "antd";
+import Icon from '@ant-design/icons';
 import Axios from "axios";
 function FileUpload(props) {
   const [Images, setImages] = useState([]);
