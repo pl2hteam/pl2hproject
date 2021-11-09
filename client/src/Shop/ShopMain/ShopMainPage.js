758d12673f076632e1d72ec38a79e4402c29ffe1
park jun hyuk <junhyeg08@gmail.com>
2021-11-09 16:51:34 +0900

    작은 오류 해결

--- client/src/Shop/ShopMain/ShopMainPage.js
+++ client/src/Shop/ShopMain/ShopMainPage.js
@@ -21,42 +21,23 @@ const ShopMainPage = () => {
   });
   const [SearchTerm, setSearchTerm] = useState("");
 
-  /* 삼품 목록 표기 (미완) */
-  const renderCards = Products.map((product, index) => {
-    return (
-        <Col lg={6} md={8} xs={24}
-          key={index} // 단순히 에러를 문구를 없얘기 위해 넣음
-        >
-          <div style={{ position: 'relative' }}>
-            <Card cover={<ImageSlider images={product.images}/>} >
-              <Meta 
-                title={product.title}
-                description={`${product.price}`}
-              />
-            </Card>
-          </div>
-        </Col>
-    )
-  })
-
-  /* 상품 목록 불러오기 */
+  // 상품목록 불러오기
   const getProducts = (body) => {
-    Axios.post("/api/mongo/product/getProducts", body)
-    .then(response => {
+    Axios.post("/api/mongo/product/getProducts", body).then((response) => {
       if (response.data.success) {
-        if(body.loadMore) {
-          setProducts([...Products, ...response.data.productInfo]);
+        if (body.loadMore) {
+          setProducts([...Products, ...response.data.products]);
         } else {
-          setProducts(response.data.productInfo);
+          setProducts(response.data.products);
         }
         setPostSize(response.data.postSize);
       } else {
-        alert("목록을 불러오는데 실패했습니다.");
+        alert("Failed to fectch product datas");
       }
-    })
-  }
+    });
+  };
 
-  /* 더보기 버튼 */
+  // 더보기 버튼
   const loadMoreHandler = () => {
     let skip = Skip + Limit;
 
@@ -64,24 +45,43 @@ const ShopMainPage = () => {
       skip: skip,
       limit: Limit,
       loadMore: true,
-    }  
+      filters: Filters,
+      searchTerm: SearchTerm,
+    };
 
     getProducts(variables);
     setSkip(skip);
-  }
+  };
 
-  /* 목록 검색 */
+  const renderCards = Products.map((product, index) => {
+    return (
+      <Col lg={3} md={4} xs={8}>
+        <Card
+          hoverable={true}
+          cover={
+            <a href={`/shop/product/${product._id}`}>
+              {" "}
+              <ImageSlider images={product.images} />
+            </a>
+          }
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
   const showFilteredResults = (filters) => {
     let body = {
-      skip: 0,    // 처음엔 아무 선택 없음
+      skip: 0, // 처음엔 아무 선택 없음
       limit: Limit,
-      filters: filters
-    }
-    getProducts(body)
-    setSkip(0)
-  }
+      filters: filters,
+    };
+    getProducts(body);
+    console.log(body);
+    setSkip(0);
+  };
 
-  /* 라디오버튼 검색 */
+  // 라디오버튼 검색
   const handlePrice = (value) => {
     const data = price;
     let array = [];
@@ -92,87 +92,105 @@ const ShopMainPage = () => {
       }
     }
     return array;
-  } 
+  };
 
   // category 는 체크박스랑 라디오 박스를 나누기 위한 것
   const handleFilters = (filters, category) => {
-    console.log('filters', filters);
     const newFilters = { ...Filters };
     newFilters[category] = filters;
-    
+
     if (category === "price") {
-      let priceValues = handlePrice(filters)
+      let priceValues = handlePrice(filters);
       newFilters[category] = priceValues;
+      console.log(newFilters);
     }
 
     showFilteredResults(newFilters);
     setFilters(newFilters);
-  }
+  };
 
-  /* 텍스트 검색 */
+  // 텍스트 검색
   const updateSearchTerm = (newSearchTerm) => {
     let body = {
       skip: 0,
       limit: Limit,
       filters: Filters,
       searchTerm: newSearchTerm,
-    }
+    };
 
     setSkip(0);
     setSearchTerm(newSearchTerm);
     getProducts(body);
-  }
+  };
 
-  /* default */
+  // default
   useEffect(() => {
     let variables = {
       skip: Skip,
       limit: Limit,
-    }  
-    
+    };
+
     getProducts(variables);
-  }, [])
+  }, []);
 
   return (
-    <div style={{ width:'75%', margin: '3rem auto' }}>
-      <div style={{ textAlign: 'center' }}>
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
         <h2>상품 메인 화면</h2>
       </div>
 
+      {/* 상품, 가격 필터 */}
       <Row gutter={[16, 16]}>
         <Col lg={12} xs={24}>
-          <CheckBox 
-            list={itemNumber} 
-            handleFilters={filters => handleFilters(filters, "itemNumber")} 
+          <CheckBox
+            list={itemNumber}
+            handleFilters={(filters) => handleFilters(filters, "itemNumber")}
           />
         </Col>
         <Col lg={12} xs={24}>
-          <Radiobox 
+          <Radiobox
             list={price}
-            handleFilters={filters => handleFilters(filters, "price")} 
+            handleFilters={(filters) => handleFilters(filters, "price")}
           />
         </Col>
       </Row>
-    
-      <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '1rem auto' }}>        
-        <SearchFeature 
-          refreshFunction={updateSearchTerm}
-        />
+
+      {/* 검색란 */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          margin: "1rem auto",
+        }}
+      >
+        <SearchFeature refreshFunction={updateSearchTerm} />
       </div>
 
-      <Row gutter={[16, 16]}>
-        {renderCards}
-      </Row>
-
+      {/* 등록된 상품이 0개면 "상품없다고 출력  */}
+      {Products.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            height: "300px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h2>등록된 상품이 없읍니다</h2>
+        </div>
+      ) : (
+        // 상품 있으면 목록 출력
+        <Row gutter={[16, 16]}>{renderCards}</Row>
+      )}
       <br />
 
-      {PostSize >= Limit && 
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {PostSize >= Limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
           <button onClick={loadMoreHandler}>더보기</button>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 };
 
 export default ShopMainPage;
