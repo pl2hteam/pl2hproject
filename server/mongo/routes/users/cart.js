75e0e34dd2cd33a07ea13057ebba1c95dcf6b90a
LeessangMin <esm2103@gmail.com>
2021-11-14 11:47:24 +0900

    장바구니 삭제기능

--- server/mongo/routes/users/cart.js
+++ server/mongo/routes/users/cart.js
@@ -1,55 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../../schemas/User");
+const { Product } = require("../../schemas/Product");
 const { auth } = require("../../middleware/auth");
 
-// router.get("/addToCart", (req, res) => {
-//   console.log(req.cookies.w_auth);
-//   User.find({ token: req.cookies.w_auth }, (err, userInfo) => {
-//     let duplicate;
-//     userInfo.cart.forEach((cartInfo) => {
-//       duplicate = false;
-//       if (cartInfo.id == req.query.pd_id) {
-//         duplicate = true;
-//       }
-//     });
-//     if (duplicate) {
-//       User.findOneAndUpdate(
-//         { token: req.cookies.w_auth, "cart.id": req.cookies.w_auth },
-//         { $inc: { "cart.$.quantity": 1 } },
-//         { new: true },
-//         (err, userInfo) => {
-//           if (err) return res.json({ success: false, err });
-//           res.status(200).json(userInfo.cart);
-//         }
-//       );
-//     } else {
-//       User.findOneAndUpdate(
-//         { token: req.cookies.w_auth },
-//         {
-//           $push: {
-//             cart: {
-//               token: req.cookies.w_auth,
-//               quantity: 1,
-//               date: Date.now(),
-//             },
-//           },
-//         },
-//         { new: true },
-//         (err, userInfo) => {
-//           if (err) return res.json({ success: false, err });
-//           res.status(200).json(userInfo.cart);
-//         }
-//       );
-//     }
-//   });
-// });
+// 장바구니에 추가
 router.get("/addToCart", auth, (req, res) => {
+  // User DB에서 사용자의 _id를 찾아서 그 DB 내용 전체를
+  // userInfo에 때려넣음
   User.findOne({ _id: req.user._id }, (err, userInfo) => {
     let duplicate = false;
 
-    console.log(userInfo);
-
+    // 사용자DB의 장바구니에 들어있는 상품종류만큼
     userInfo.cart.forEach((item) => {
       if (item.id == req.query.productId) {
         duplicate = true;
@@ -102,7 +64,7 @@ router.get("/removeFromCart", auth, (req, res) => {
       });
 
       Product.find({ _id: { $in: array } })
-        .populate("writer")
+        .populate("seller")
         .exec((err, cartDetail) => {
           return res.status(200).json({
             cartDetail,
@@ -121,7 +83,7 @@ router.get("/userCartInfo", auth, (req, res) => {
     });
 
     Product.find({ _id: { $in: array } })
-      .populate("writer")
+      .populate("seller")
       .exec((err, cartDetail) => {
         if (err) return res.status(400).send(err);
         return res.status(200).json({ success: true, cartDetail, cart });
