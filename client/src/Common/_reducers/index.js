f5d35962129ac036bf2942daae8e33eb72744ba6
park jun hyuk <junhyeg08@gmail.com>
2021-11-14 13:29:45 +0900

    병합 오류 수정

--- client/src/Common/_reducers/index.js
+++ client/src/Common/_reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import user from './user_reducer';
 import playlist from './playlist';
 
--- client/src/SNS/Profile/Intro/MovieFileUpload.js
+++ client/src/SNS/Profile/Intro/MovieFileUpload.js
@@ -1,4 +1,3 @@
-=======
 import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import Axios from "axios";
