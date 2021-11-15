a265168986383dc38171e2833af8c3f8588f9454
codecoco <88923210+codecocos@users.noreply.github.com>
2021-11-15 23:56:57 +0900

    병합전

--- client/src/Common/components/SNSImageSlider .js
+++ client/src/Common/components/SNSImageSlider .js
@@ -2,11 +2,13 @@ import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 
 function ImageSlider(props) {
-  console.log(props);
+  console.log("SNSImageSlider:", props);
+  console.log("SNSImageSlider : images", props.images);
   return (
     <div>
       <div showArrows={true}>
         {props.images.images.map((image, index) => (
+
           <div key={index}>
             <img
               style={{ width: "100%", maxHeight: "500px" }}
