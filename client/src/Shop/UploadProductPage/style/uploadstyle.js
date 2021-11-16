907d99b2466c1b2fa19269c66dece3a2cdf63596
park jun hyuk <junhyeg08@gmail.com>
2021-11-16 12:29:07 +0900

    업로드 페이지 디자인

--- client/src/Shop/UploadProductPage/style/uploadstyle.js
+++ client/src/Shop/UploadProductPage/style/uploadstyle.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const UploadStyle = styled.div`
+  {
+    margin: -30px
+  } 
+
   .ant-form {
     top: 0px;
   }
@@ -9,7 +13,11 @@ export const UploadStyle = styled.div`
     margin-left: 15vw;
   }
 
-  input[type = text] {
+  textarea, 
+  input[type = text], 
+  input[type = number], 
+  select,
+  button  {
     background: transparent;
     border-color: RGBA(0,0,0,0.3);
     width: 80%;
@@ -18,8 +26,34 @@ export const UploadStyle = styled.div`
     border-radius: 10px;
   }
 
-  input:focus {
+  input:focus, 
+  textarea:focus, 
+  select {
     outline: none;
     box-shadow: 0 0 0 1px RGBA(0,0,0,0.3);
+  }
+  
+  input[type = text], input[type = number] {
+    padding-left: 5px;
+  }
+
+  textarea {
+    border: 1.9px solid;
+    width: 90%;
+    height: 5vw;
+    margin-top: 10px;
+    padding-top: 5px;
+  }
+
+  button {
+    border: 2.1px solid RGBA(0,0,0,0.3);
+    margin-top: 15px;
+    margin-left: 120px;
+    width: 170px;
+    height: 40px;
+  }
+
+  button:hover {
+    border: 3.5px solid RGBA(0,0,0,0.3);
   }
 `;
