cc062e2376248355ba36552dee1f2b5f907659c1
LeessangMin <esm2103@gmail.com>
2021-11-18 19:39:57 +0900

    APP.js 메인페이지 / 상점 / SNS 분리, 메인페이지 디자인 등..

--- client/src/Common/App.js
+++ client/src/Common/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import SNSMain from "./SNSMain";
 import ShopMain from "../Shop/MainForm/ShopMain";
+import MainPage from "./MainPage";
 
 function App() {
-  
   return (
-    <>
+    <div>
+      <MainPage />
       <SNSMain />
       <ShopMain />
-    </>
+    </div>
   );
 }
 
