ff7cc7eb3d594b700ba6c968ba4f7236b15341d7
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-11-21 21:24:48 +0900

    sdd

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.get("*", (req, res) => {//
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
