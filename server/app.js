ed6c6bbbfaeba7227edcf84c8dc86ac89a41dd0a
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-11-21 20:20:18 +0900

    dd

--- server/app.js
+++ server/app.js
@@ -9,17 +9,15 @@ const passportConfig = require("./mysql/passport");
 dotenv.config();
 
 
-
 /* DB 라우터 */
 const mysqlRouter = require("./mysql/routes");
 const mongoRouter = require("./mongo/routes");
 const passport = require("passport");
 
-
-
+const app = express();
 passportConfig();
 const cors = require("cors");
-// app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
 /* 시퀄라이즈 연결 */
 sequelize
@@ -32,8 +30,8 @@ sequelize
   });
 
 /* 몽고 DB 연결 */
-const connect = require("./mongo/schemas");
-connect();
+// const connect = require("./mongo/schemas");
+// connect();
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -71,21 +69,17 @@ app.use("/api/mongo", mongoRouter);
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
-  return res.status(400).send(err);
+  next(error);
 });
+
 
 /* error 처리 */
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  return res.status(400).send(err);
+  res.status(500).json({ err })
 });
-const app = express();
-const http=require('http').createServer(app);
-http.listen(5000,function(){
-  console.log(`listening on 5000`);
-})
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
 });
