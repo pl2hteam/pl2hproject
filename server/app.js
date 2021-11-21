29e6da5041ad7a39efff8f62b397247487dde2f0
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-11-21 18:43:44 +0900

    ㅁㅇㅇ

--- server/app.js
+++ server/app.js
@@ -72,13 +72,13 @@ app.use((req, res, next) => {
 });
 
 /* error 처리 */
+/* error 처리 */
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render("error");
+  res.status(500).json({ err })
 });
-
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
 });
